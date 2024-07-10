65cabe45a4ec00447efa8ff97a5c3a444cb79159
David Amparan <43897297+daamparan@users.noreply.github.com>
2024-07-09 20:43:45 -0400

    Correction to app bar

--- app/src/main/java/com/zenith/dev/netnation/ui/components/molecules/MoleculeTopAppBar.kt
+++ app/src/main/java/com/zenith/dev/netnation/ui/components/molecules/MoleculeTopAppBar.kt
@@ -2,18 +2,26 @@
 
 package com.zenith.dev.netnation.ui.components.molecules
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,39 +30,44 @@ import com.zenith.dev.netnation.R
 import com.zenith.dev.netnation.ui.components.atoms.AtomText
 import com.zenith.dev.netnation.ui.theme.NetNationTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoleculeTopAppBar(
-    title: String,
-    onBackClick: () -> Unit,
-    icon: ImageVector? = null,
-    label: String? = "",
-    buttonTitle: String = "",
-    onButtonClick: () -> Unit
+    modifier: Modifier = Modifier,
+    @StringRes titleResource: Int,
+    navigationIcon: ImageVector,
+    navigationIconContentDescription: String,
+    actionIcon: ImageVector,
+    actionIconContentDescription: String,
+    onNavigationIconClicked: () -> Unit = {},
+    onActionIconClicked: () -> Unit = {},
+    testTagIdentifier: String = "MoleculeTopAppBar"
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    AtomText(
-                        text = title,
-                        style = MaterialTheme.typography.headlineLarge,
-                        fontWeight = FontWeight.SemiBold,
-                    )
-                },
-                navigationIcon = {
-                    icon?.let {
-                        Icon(
-                            imageVector = it,
-                            contentDescription = label ?: "Icon"
-                        )
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        Modifier
-            .padding(innerPadding)
-    }
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(id = titleResource)) },
+        navigationIcon = {
+            IconButton(onClick = { onNavigationIconClicked() }) {
+                Icon(
+                    imageVector = navigationIcon,
+                    contentDescription = navigationIconContentDescription,
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { onActionIconClicked() }) {
+                Icon(
+                    imageVector = actionIcon,
+                    contentDescription = actionIconContentDescription,
+                    modifier = Modifier
+                        .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+                )
+            }
+        },
+        modifier = modifier
+            .testTag(tag = testTagIdentifier)
+    )
 }
 
 @Preview(showBackground = true)
@@ -62,13 +75,11 @@ fun MoleculeTopAppBar(
 fun MoleculeTopAppBarPreview() {
     NetNationTheme {
         MoleculeTopAppBar(
-            title = "Title",
-            onBackClick = {},
-            icon = Icons.Default.Search,
-            buttonTitle = "Button",
-            onButtonClick = {
-
-            }
+            titleResource = R.string.app_name,
+            navigationIcon = Icons.Filled.Menu,
+            navigationIconContentDescription = "Search",
+            actionIcon = Icons.Filled.Search,
+            actionIconContentDescription = "Search"
         )
     }
 }
