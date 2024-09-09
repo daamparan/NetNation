84edc6b2f63394190fbb025de6606810a708315a
David Amparan <43897297+daamparan@users.noreply.github.com>
2024-09-08 20:11:52 -0400

    Adding Retrofit
    - Added Retrofit for network connections

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.dagger:hilt-android:2.51")
     kapt("com.google.dagger:hilt-android-compiler:2.51")
+    implementation("com.squareup.retrofit2:retrofit2:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     // Test Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
